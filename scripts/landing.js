012e54b33ff68cc1975e01f21b59e10566509884
concept-x <emilie_elliotthotmail.com>
2017-06-04 16:06:46 -0500

    Checkpoint-9-dom-scripting-events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,5 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function(points) {
   var revealPoint = function(index){
       points[index].style.opacity = 1;
       points[index].style.transform = "scaleX(1) translateY(0)";
@@ -11,4 +11,16 @@ var animatePoints = function() {
     revealPoint(i);
     }
 };
-animatePoints();
+//animatePoints();
+window.onload = function(){
+  if(window.innerHeight > 950){
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight+200;
+  window.addEventListener('scroll', function(event){
+    if (document.documentElement.scrollTop || document.body.scrollTop>=scrollDistance){
+      animatePoints(pointsArray);
+    }
+  });
+}
