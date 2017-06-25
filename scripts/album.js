4cd525384604bf5e8c2be65994291324e7eee78b
concept-x <emilie_elliotthotmail.com>
2017-06-24 23:28:24 -0500

    checkpoint-13-dom-scripting-part-2: completed

--- scripts/album.js
+++ scripts/album.js
@@ -79,30 +79,105 @@ var setCurrentAlbum = function(album){
     albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
   }
 };
+//ckpoint 13: change song# to pause button
+//starting at .song-item-number, bubble up to find a parent with a specified class name
+var findParentByClassName = function(element, targetClass) {//takes current element & target parent class args
+  if(element){//pass in element you want parent of
+    var currentParent = element.parentElement;//set cP = immediate parent of current element
+    while(currentParent.className !== targetClass && currentParent.className !== null){//while cP class !== target class and cP class exists
+      currentParent = currentParent.parentElement;//move one step up the DOM tree to find next parent
+    }
+    return currentParent;//if 'if-condition' is no longer met, return cP & exit function (we've found target parent class)
+  }
+};//end findParentByClassName function
+
+//start getSongItem method...take element w/className & return .song-item-number class using switch stmt
+var getSongItem = function (element){//I got this right by mahself!!
+  //use switch statement to return element w/.song-item-number class. Either
+  //1.the element already *is* .song-item-number, or
+  //2.element is a sibling of .song-item-number, or
+  //3. element is a parent of .song-item-number
+  switch(element.className){
+    //account for all possible relationships to .song-item-number from other album.css classes
+    case 'album-song-button':
+    case 'ion-play':
+    case 'ion-pause': //in all three cases, return below:
+      return findParentByClassName(element,'song-item-number');
+    case 'album-view-song-item':
+      return element.querySelector('.song-item-number');
+    case 'song-item-title':
+    case 'song-item-duration':
+      return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+    case 'song-item-number': //if the element *is* what we're looking for, then
+      return element;//just return it. Why isn't this the default?
+    default://answer is blank here...why?
+      return;
+  }
+};
+//end getSongItem
+//end ckpoint 13 code
+
+var clickHandler = function(targetElement) {
+  var songItem = getSongItem(targetElement);
+
+  if(currentlyPlayingSong === null){
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  } else if(currentlyPlayingSong === songItem.getAttribute('data-song-number')){
+      songItem.innerHTML = playButtonTemplate;
+      currentlyPlayingSong = null;
+  } else if(currentlyPlayingSong !== songItem.getAttribute('data-song-number')){
+      var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+      currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+      songItem.innerHTML = pauseButtonTemplate;
+      currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  }
+};
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 //Elements we'll be adding listeners to
 var songRows = document.getElementsByClassName('album-view-song-item');
 //Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+//Store state of playing songs
+var currentlyPlayingSong = null;
+
 window.onload = function(){
   setCurrentAlbum(albumPicasso);
 
 songListContainer.addEventListener('mouseover', function(event){
   //Only target individual song rows during event delegation
   if(event.target.parentElement.className === 'album-view-song-item'){
-    //Change content from song# to play button's HTML
-    event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    //Change content from song# to play button
+      
+      var songItem = getSongItem(event.target);
+
+      if(songItem.getAttribute('data-song-number') !== currentlyPlayingSong){
+        songItem.innerHTML = playButtonTemplate;
+      }
   }
 });
+//update w/conditional that only changes innerHTML of <td> when <td> !== currently playing song
 
   for(var i = 0; i<songRows.length; i++){
     songRows[i].addEventListener('mouseleave', function(event){
-      //selects first child element, song-item-number
-      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      //#1: cache the song being left in a var
+      var songItem = getSongItem(event.target);
+      var songItemNumber = songItem.getAttribute('data-song-number');
+
+      //#2: check whether mouse is leaving current song, and change songItem if not
+      if(songItemNumber !==currentlyPlayingSong){
+        songItem.innerHTML = songItemNumber;
+      }
+    });
+
+    songRows[i].addEventListener('click', function(event){
+      //event handler call
+      clickHandler(event.target);
     });
   }
-
   var albumArray = [albumPicasso, albumMarconi, albumSummer];
   var index = 1;
   albumImage.addEventListener("click", function(event){
