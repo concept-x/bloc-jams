54da32813408eb4944f2635147c61c1417c3d9ad
concept-x <emilie_elliotthotmail.com>
2017-07-08 11:21:56 -0500

    checkpoint-16-jquery-landing: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,15 @@
-var pointsArray = document.getElementsByClassName('point');
-var animatePoints = function(points) {
-  var revealPoint = function(index){
+//var pointsArray = document.getElementsByClassName('point');
+//var animatePoints = function(points) {
+  var animatePoints = function(){
+    var revealPoint = function(){
+      $(this).css({
+        opacity: 1,
+        transform: 'scaleX(1) translateY(0)'
+      });
+    };
+  $.each($('.point'), revealPoint);
+};
+  /*var revealPoint = function(index){
       points[index].style.opacity = 1;
       points[index].style.transform = "scaleX(1) translateY(0)";
       points[index].style.msTransform = "scaleX(1) translateY(0)";
@@ -9,18 +18,23 @@ var animatePoints = function(points) {
 
   for(var i = 0; i<points.length; i++)  {
     revealPoint(i);
-    }
-};
+  }*/
+
 //animatePoints();
-window.onload = function(){
-  if(window.innerHeight > 950){
-    animatePoints(pointsArray);
+$(window).load (function(){
+  if ($(window).height() > 950){
+    animatePoints();
   }
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight+200;
-  window.addEventListener('scroll', function(event){
-    if (document.documentElement.scrollTop || document.body.scrollTop>=scrollDistance){
-      animatePoints(pointsArray);
+  var scrollDistance =  $('.selling-points').offset().top - $(window).height() + 200;
+  //var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight+200;
+  //window.addEventListener('scroll', function(event){
+  $(window).scroll(function(event){
+    if($(window).scrollTop() >= scrollDistance){
+      animatePoints();
     }
   });
-}
+    //if (document.documentElement.scrollTop || document.body.scrollTop>=scrollDistance){
+      //animatePoints(pointsArray);
+    //}
+  });
